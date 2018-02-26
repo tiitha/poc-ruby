a6b4117c6a209d014b72da9e33bd4f6a5ff48bc0
Tiit Hallas <tiitha@scada.local>
2018-02-26 14:35:11 +0200

    tab -> 2-space

--- nonblock.rb
+++ nonblock.rb
@@ -9,70 +9,70 @@ require 'io/console'
 #
 
 class InputWaiter
-	def initialize()
-		@paused = false
-		@text = "running it for the %d-th time.."
-	end
+  def initialize()
+    @paused = false
+    @text = "running it for the %d-th time.."
+  end
 
-	def paused()
-		return @paused
-	end
+  def paused()
+    return @paused
+  end
 
-	def text()
-		return @text+"\n"
-	end
+  def text()
+    return @text+"\n"
+  end
 
-	def wait_for_input()
-		cmd = ""
-		loop do
-			ch = STDIN.getch
-			if ch.ord == 3 # (Ctrl + c)
-				if @paused
-					cmd = ""
-					@paused = false
-				else
-					exit
-				end
+  def wait_for_input()
+    cmd = ""
+    loop do
+      ch = STDIN.getch
+      if ch.ord == 3 # (Ctrl + c)
+        if @paused
+          cmd = ""
+          @paused = false
+        else
+          exit
+        end
 
-			end
-			if ch == ":"
-				@paused = true
-				cmd = ""
-				print "run: "
-			else
-				cmd += ch
-				print ch if @paused
-				if ch.ord == 13
-					parse_cmd(cmd) if cmd.length > 1
-					cmd = ""
-					sleep(1)
-					@paused = false
-				end
-			end
-		end 		
-	end
+      end
+      if ch == ":"
+        @paused = true
+        cmd = ""
+        print "run: "
+      else
+        cmd += ch
+        print ch if @paused
+        if ch.ord == 13
+          parse_cmd(cmd) if cmd.length > 1
+          cmd = ""
+          sleep(1)
+          @paused = false
+        end
+      end
+    end     
+  end
 
-	def parse_cmd(cmd)
-		puts "command to execute: "+cmd
-		if cmd.start_with?("text ")
-			@text = cmd[5..-2]
-		end
-	end
+  def parse_cmd(cmd)
+    puts "command to execute: "+cmd
+    if cmd.start_with?("text ")
+      @text = cmd[5..-2]
+    end
+  end
 end
 
 
 iw = InputWaiter.new()
 
 Thread.new { 
-	iw.wait_for_input()
+  iw.wait_for_input()
 }
 
 i = 0
 loop do
-	unless iw.paused()
-	  system("clear")
-	  printf iw.text(), i
-	  i += 1
-	end
+  unless iw.paused()
+    system("clear")
+    printf iw.text(), i
+    i += 1
+  end
   sleep 1
 end
