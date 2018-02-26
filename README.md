fc813e193fd85439bbfe7a6ed2c6ac72d0b8107d
Tiit Hallas <tiitha@scada.local>
2018-02-26 14:32:24 +0200

    adding nonblock

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # poc-ruby
 POC scripts and other testing scripts written in ruby
+## nonblock.rb
+'nonblock.rb' enables me to interact with already running processes, e.g. manipulate dictionary content of my running scripts
 ## xtail.rb
 'tail -f' functionality implemented in ruby that enables you to tail multiple files simultaniously based on a file pattern. Script scans the changes on your disk based on the pattern and adds (or removes) the respective files. In other words - the files you want to monitor might not exist yet.
---
+++ nonblock.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+require 'io/console'
+
+#
+# ugly as f*ck but does the trick (w. some exceptions, e.g. backspace)
+# loop that refreshes the screen until user cancelles it (ctrl+c) and takes commands in-between
+# w. ':' prefix (e.g. ':text "blaah"')
+# enables me to interact with already started processes 
+#
+
+class InputWaiter
+	def initialize()
+		@paused = false
+		@text = "running it for the %d-th time.."
+	end
+
+	def paused()
+		return @paused
+	end
+
+	def text()
+		return @text+"\n"
+	end
+
+	def wait_for_input()
+		cmd = ""
+		loop do
+			ch = STDIN.getch
+			if ch.ord == 3 # (Ctrl + c)
+				if @paused
+					cmd = ""
+					@paused = false
+				else
+					exit
+				end
+
+			end
+			if ch == ":"
+				@paused = true
+				cmd = ""
+				print "run: "
+			else
+				cmd += ch
+				print ch if @paused
+				if ch.ord == 13
+					parse_cmd(cmd) if cmd.length > 1
+					cmd = ""
+					sleep(1)
+					@paused = false
+				end
+			end
+		end 		
+	end
+
+	def parse_cmd(cmd)
+		puts "command to execute: "+cmd
+		if cmd.start_with?("text ")
+			@text = cmd[5..-2]
+		end
+	end
+end
+
+
+iw = InputWaiter.new()
+
+Thread.new { 
+	iw.wait_for_input()
+}
+
+i = 0
+loop do
+	unless iw.paused()
+	  system("clear")
+	  printf iw.text(), i
+	  i += 1
+	end
+  sleep 1
+end
